6e1a62f23d789ef293ecd4bf80a3a6f4b27b3e5f
Gurparas Singh <gurparas0248.be20@chitkara.edu.in>
2023-01-12 17:24:44 +0530

    reverse a string using a Stack

--- Introduction/Pushatbottom.java
+++ Introduction/Pushatbottom.java
@@ -12,15 +12,24 @@ public class Pushatbottom {
         pushAtBottom(s,data);
         s.push(top);
     }
-    public static void main(String[] args) {
-        Stack<Integer> s=new Stack<>();
-        s.push(1);
-        s.push(2);
-        s.push(3);
 
-        pushAtBottom(s,4);
-        while(!s.isEmpty()){
-            System.out.println(s.pop());
+    public static String reverseString(String str){
+        Stack<Character> s=new Stack<>();
+        int idx=0;
+        while(idx<str.length()){
+            s.push(str.charAt(idx));
+            idx++;
         }
+        StringBuilder result=new StringBuilder("");
+        while(!s.isEmpty()){
+            char curr=s.pop();
+            result.append(curr);
+        }
+        return result.toString();
+    }
+    public static void main(String[] args) {
+        String str="abc";
+        String result=reverseString(str);
+        System.out.println(result);
     }
 }
